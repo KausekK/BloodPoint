0cd5a2698678f99c6748475a1c5a041b01b1052f
kausek <jakub.kausa@gmail.com>
2025-06-13 22:44:09 +0200

    init backend

---
+++ backend/src/main/java/com/point/blood/BloodPointApplication.java
@@ -0,0 +1,13 @@
+package com.point.blood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodPointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodPointApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/point/blood/BloodPointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.point.blood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodPointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
