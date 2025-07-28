7eca51ccef135a12a99eb4bbc6e782a8629f5c78
kausek <jakub.kausa@gmail.com>
2025-07-28 19:14:54 +0200

    forecast of blood demand chart

---
+++ frontend/src/services/HospitalService.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "/api/hospital",
+  timeout: 8000,
+});
+
+export function getHospitalsProvinces() {
+ return api
+    .get("/provinces")
+    .then((r) => r.data);
+}
