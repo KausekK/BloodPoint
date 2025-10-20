66825d62810d45350ea4704a5241a81cfa11d10c
Aleksandra <a.filipowicz12345@gmail.com>
2025-10-20 20:43:55 +0200

    remove column in Questionnaire table created_at

--- backend/src/main/java/com/point/blood/questionnaire/Questionnaire.java
+++ backend/src/main/java/com/point/blood/questionnaire/Questionnaire.java
@@ -26,9 +26,6 @@ public class Questionnaire {
     @Column(nullable = false)
     private String description;
 
-    @Column(name = "created_at", nullable = false)
-    private LocalDateTime createdAt;
-
     @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
 }
